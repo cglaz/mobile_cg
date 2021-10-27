c68e76dee7ad354679c1db1b6aac3ead22d42662
cglaz <cezazzrg>
2021-10-27 20:30:49 +0200

    Exercise 2

---
+++ PhoneWord/PhoneWord/SharedResources.cs
@@ -0,0 +1,16 @@
+using Xamarin.Forms;
+
+namespace PhoneWord {
+    
+public static class SharedResources
+{
+    public static Color ButtonBkColor {
+        get { return Color.FromRgb(0xff, 0xa5, 0); }
+    }
+
+    public static Color ButtonTextColor {
+        get { return Color.White; }
+    }
+}
+
+}
